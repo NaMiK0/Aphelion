c0a3c8d7be2870d1ea7145021902da4065801321
Kitolov <nikita.khaziev@mail.ru>
2026-03-18 12:05:13 +0300

    Initial Commit

---
+++ Aphelion/AphelionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AphelionApp.swift
+//  Aphelion
+//
+//  Created by Nikita on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct AphelionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AphelionTests/AphelionTests.swift
@@ -0,0 +1,17 @@
+//
+//  AphelionTests.swift
+//  AphelionTests
+//
+//  Created by Nikita on 18.03.2026.
+//
+
+import Testing
+@testable import Aphelion
+
+struct AphelionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
