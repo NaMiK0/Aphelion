5c1a5f7e359f0d70b4f9aae091e70ccb790f3f0e
Kitolov <nikita.khaziev@mail.ru>
2026-03-18 13:17:05 +0300

    add APOD model

---
+++ Aphelion/Features/APOD/Models/APOD.swift
@@ -0,0 +1,9 @@
+struct APOD: Codable {
+    let title: String
+    let date: String
+    let explanation: String
+    let url: String
+    let hdurl: String?
+    let mediaType: String
+    let copyright: String?
+}
