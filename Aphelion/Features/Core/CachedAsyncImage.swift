3cfe02b486bdbc738e53cc939894a07151ad856e
Kitolov <nikita.khaziev@mail.ru>
2026-03-22 21:07:22 +0300

    feat: replace URLCache with NSCache for image caching

--- Aphelion/Features/Core/CachedAsyncImage.swift
+++ Aphelion/Features/Core/CachedAsyncImage.swift
@@ -1,8 +1,12 @@
 import SwiftUI
+
+private let imageCache = NSCache<NSURL, UIImage>()
+
 struct CachedAsyncImage<Content: View, Placeholder: View>: View {
     let url: URL?
     let content: (Image) -> Content
     let placeholder: () -> Placeholder
+    
     
     @State private var uiImage: UIImage? = nil
     
@@ -15,11 +19,18 @@ struct CachedAsyncImage<Content: View, Placeholder: View>: View {
             }
         }
         .task(id: url) {
-            uiImage = nil
             guard let url else { return }
-            guard let (data, _) = try? await URLSession.shared.data(from: url) else { return }
-            
-            uiImage = UIImage(data: data)
+
+            if let cached = imageCache.object(forKey: url as NSURL) {
+                uiImage = cached
+                return
+            }
+
+            guard let (data, _) = try? await URLSession.shared.data(from: url),
+                  let image = UIImage(data: data) else { return }
+
+            imageCache.setObject(image, forKey: url as NSURL)
+            uiImage = image
         }
     }
 }
